12e40094f15e92354ef996af4eb84ee76e10fee2
A. Noval Adip <a.novaladip@yahoo.com>
2020-05-01 01:05:36 +0700

    add teacher_only middleware to attendance bulk update

--- afc_core/api/attendance/controller.py
+++ afc_core/api/attendance/controller.py
@@ -3,10 +3,12 @@ from flask import Blueprint, jsonify,  request
 from .repository import AttendanceRepository
 
 from afc_core.models.attendance import Attendance
+from afc_core.middleware.auth import teacher_only
 
 attendance = Blueprint('attendance', __name__)
 
 
+@teacher_only
 @attendance.route('/update/bulk', methods=['PUT'])
 def update():
     data = request.json
